9e2198b6105b94dd2e7ab3a6bafc4c9c62cad571
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-03 09:57:22 +0000

    create levels

--- js/script.js
+++ js/script.js
@@ -12,44 +12,34 @@ const select = document.getElementById('levels');
 const mainSquare = document.getElementById('square');
 const playButton = document.getElementById('btn-play');
 
-let easySquare = document.querySelector('.easy-square');
-let mediumSquare = document.querySelector('.medium-square');
-let hardSquare = document.querySelector('.hard-square');
+let arrLevels = [100, 81, 49];
+
+playButton.addEventListener('click', setupGame);
+
+function setupGame() {
+
+    mainSquare.innerHTML = '';
+    const indexLevels = parseInt(select.value);
+
+    const cellsCount = arrLevels[indexLevels];
+
+    const cellParRow = Math.sqrt(cellsCount);
+
+    for (let cellNum = 1; cellNum <= cellsCount; cellNum++) {
+        console.log(cellNum);
+
+        let box = document.createElement('div');
+        box.classList.add('box');
+        box.append(cellNum);
+        box.style.width = `calc(100% / ${cellParRow})`;
+        box.style.height = `calc(100% / ${cellParRow})`;
 
 
-let box;
-
-let easy = 101;
-let medium = 81;
-let hard = 51;
-
-createSquare(box, easy, easySquare);
-createSquare(box, medium, mediumSquare);
-createSquare(box, hard, hardSquare);
-
-
-clickPlay(playButton, easySquare, mediumSquare, hardSquare);
-clickPlay(playButton, mediumSquare, easySquare, hardSquare);
-clickPlay(playButton, hardSquare, mediumSquare, easySquare);
-
-
-// function click add and remove class
-function clickPlay (button, add, remove1, remove2) {
-    button.addEventListener('click', function() {
-        add.classList.add('active')
-        remove1.classList.remove('active')
-        remove2.classList.remove('active')
-    })
-}
-
-
-//function boxes
-function createSquare (element, maxBoxes, square) {
-    for (let i = 1; i < maxBoxes; i++) {
-        element = document.createElement('div');
-        element.classList.add('box');
-        element.innerHTML = i;
+        // box.addEventListener('click', changeColor);
     
-        square.append(element);
+        square.append(box);
     }
+
 }
+
+
